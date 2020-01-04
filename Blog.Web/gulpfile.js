4b430e0c980c71aca3171f5448eaa11ba7f64d2f
mike.towill <29863888+zola-25@users.noreply.github.com>
2020-01-04 15:06:53 +0000

    Upgrades

--- Blog.Web/gulpfile.js
+++ Blog.Web/gulpfile.js
@@ -2,11 +2,16 @@
 
 var gulp = require("gulp");
 var sass = require('gulp-sass');
+var lec = require('gulp-line-ending-corrector');
+var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
 var del = require('del');
 var config = require('./gulpconfig.json');
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
-
-gulp.task("default", ['libraries', 'sass'], function () { });
+gulp.task("default", ['libraries', 'sass', 'minify-css', 'babel-transpile'], function () { });
 
 gulp.task("clean-lib", function () {
     return del(['wwwroot/lib/']);
@@ -15,6 +20,11 @@ gulp.task("clean-lib", function () {
 gulp.task("clean-css", function () {
     return del(['wwwroot/css/*.css']);
 });
+
+gulp.task("clean-js", function () {
+    return del(['wwwroot/js/**/*.min.js', 'wwwroot/js/**/*.min.js.map' ]);
+});
+
 
 gulp.task("libraries", ['clean-lib'], function () {
     return gulp.src(config.libraries.src, { base: "node_modules" })
@@ -25,6 +35,30 @@ gulp.task("sass", ['clean-css'], function () {
     return gulp.src('wwwroot/css/**/*.scss', { base: "." })
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('minify-css', ['sass'], () => {
+    return gulp.src('wwwroot/css/**/*.css', { base: "." })
+        .pipe(cleanCSS())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('babel-transpile', ['clean-js'], function () {
+    return gulp.src([
+            'wwwroot/js/**/*.js',
+            'wwwroot/js/**/*.jsx'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(uglify({ mangle: false }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('wwwroot/js'));
 });
 
 gulp.task('sass:watch', function () {
